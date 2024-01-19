bbf3982e8d7eaecf7b4b5ffa79b6a59fe62de212
Osni Pezzini Junior <osni.pezzini@gmail.com>
2024-01-19 15:48:12 -0300

    Criado lista tipada com carregamento sob demanda

---
+++ lib/src/views/list.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
+
+class SOListView<TData> extends StatefulWidget {
+  const SOListView({
+    Key? key,
+    required this.items,
+    required this.itemBuilder,
+    this.loadMore,
+    this.header,
+  }) : super(key: key);
+
+  final List<TData> items;
+  final Future<List<TData>> Function()? loadMore;
+  final Widget? Function(TData) itemBuilder;
+  final Widget? header;
+
+  @override
+  _SOListViewState<TData> createState() => _SOListViewState<TData>();
+}
+
+class _SOListViewState<TData> extends State<SOListView<TData>> {
+  final logger = Logger();
+  final controller = ScrollController();
+
+  List<TData> _items = [];
+  bool _isLoadMoreRunning = false;
+  bool _hasNextPage = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _items = widget.items;
+    controller.addListener(scrollingEnds);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        if (widget.header != null)
+          SizedBox(
+            height: 50,
+            child: widget.header,
+          ),
+        Padding(
+          padding: const EdgeInsets.only(top: 50),
+          child: Scrollbar(
+            controller: controller,
+            child: ListView.builder(
+              controller: controller,
+              itemBuilder: (_, index) => widget.itemBuilder(_items[index]),
+              itemCount: _items.length,
+            ),
+          ),
+        ),
+        if (_isLoadMoreRunning)
+          const Padding(
+            padding: EdgeInsets.only(top: 10, bottom: 40),
+            child: Center(child: CircularProgressIndicator()),
+          ),
+      ],
+    );
+  }
+
+  void showSnackBar(String message) {
+    final snackBar = SnackBar(content: Text(message));
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+  void loadMore() async {
+    if (!_hasNextPage || _isLoadMoreRunning || widget.loadMore == null) return;
+
+    setState(() {
+      _isLoadMoreRunning = true;
+    });
+    try {
+      final fetchedData = await widget.loadMore!();
+      if (fetchedData.isEmpty) {
+        _hasNextPage = false;
+        showSnackBar('Nada mais a carregar!');
+      } else {
+        setState(() {
+          _items.addAll(fetchedData);
+        });
+      }
+    } catch (e) {
+      logger.e('Erro ao carregar dados: $e');
+    }
+    setState(() {
+      _isLoadMoreRunning = false;
+    });
+  }
+
+  void scrollingEnds() {
+    if (controller.position.extentAfter < 300) {
+      loadMore();
+    }
+  }
+}
